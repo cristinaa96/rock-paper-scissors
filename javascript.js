677626c3e60af5212f48256245c5e8299cd41862
Cristina Arguello <26153427+cristinaa96@users.noreply.github.com>
2023-03-05 23:17:19 -0500

    Created a function that outputs the words rock, paper, or scissors randomly

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
 function getComputerChoice(number){
-   return number;
+    let i = Math.floor(Math.random() * 3);
+    let outcome = i;
+
+    if (i === 0) {
+        outcome = "Rock";
+    } else if (i === 1){
+        outcome = "Paper";
+    }
+    else if(i === 2){
+        outcome = "Scissors";
+    } else{
+        outcome = "Invalid";
+    }
+    number = outcome;
+    return number;
 }
-console.log(getComputerChoice(2));
+console.log(getComputerChoice());
