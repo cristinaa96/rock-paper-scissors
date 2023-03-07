830f94141f372faa3a83ee329ddf1a77e5433600
Cristina Arguello <26153427+cristinaa96@users.noreply.github.com>
2023-03-06 21:03:48 -0500

    In process of adding in a game function that will loop 5 times and keep track of the score. Currently stuck here.

--- javascript.js
+++ javascript.js
@@ -15,26 +15,49 @@ function getComputerChoice(){
     return outcome;
 }
 function playRound(playerSelection,computerSelection) {
-    let gameResult = "No one";
+    let roundResult = "No one";
    let playSel = playerSelection.toUpperCase();
    let compSel = computerSelection.toUpperCase();
 
-   if (playSel === compSel){
-    gameResult = "Tie!";
-   } else if (playSel === "ROCK" && compSel === "SCISSORS" || playSel === "PAPER" && compSel === "ROCK" || playSel === "SCISSORS" && compSel === "PAPER"){
-    gameResult = "Player wins!"
-   }   
-   else {
-    gameResult = "Computer wins!";
+   if (playSel === "ROCK" && compSel === "SCISSORS"){
+        roundResult = "Player wins! Rock beats scissors!";
+   } else if (playSel === "ROCK" && compSel === "PAPER"){
+        roundResult = "Computer wins! Paper beats rock!"
+   } else if (playSel === "PAPER" && compSel === "ROCK"){
+        roundResult = "Player wins! Paper beats rock!";
+   } else if (playSel === "PAPER" && compSel === "SCISSORS") {
+        roundResult = "Computer wins! Scissors beats paper";
+   } else if (playSel === "SCISSORS" && compSel === "PAPER"){
+        roundResult = "Player wins! Scissors beats paper";
+   } else if (playSel === "SCISSORS" && compSel === "ROCK"){
+        roundResult = "Computer wins! Rock beats scissors!"
+   } else if (playSel === compSel){
+        roundResult = "Tie!";
+   } else {
+        roundResult = "Invalid input!";
    }
-
-
-    return gameResult;
+    return roundResult;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection,computerSelection));
+function game(gameResult){
+    let round = playRound(playerSelection,computerSelection);
+    for (let i = 0; i < 5; i++){
+        round;
+        //console.log("This has been looped " + round + " times.");
+    }
+    return "The result is: " + round;
+}
+
+
+
+//const playerSelection = "rock";
+let inputPrompt = prompt("Please enter rock, paper, or scissors");
+let playerSelection = inputPrompt;
+
+const computerSelection = getComputerChoice();
+console.log(game());
+
+console.log("Player selected: " + playerSelection);
+console.log("Computer selected: " + computerSelection);
+console.log(playRound(playerSelection,computerSelection)); 
